b10b8b12dca67cd177d6b22cabc2551252b198e3
Rajguru Parambath <rajguruparambath@gmail.com>
2025-06-21 16:49:00 +0530

    update dynamic url, add redirects

--- monthly_challanges/challanges/urls.py
+++ monthly_challanges/challanges/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-   path("<int:month>",views.monthly_challenge_by_number),
-   path("<str:month>",views.monthly_challenge, name='month-challenge')
+    path("",views.index),
+    path("<int:month>",views.monthly_challenge_by_number),
+    path("<str:month>",views.monthly_challenge, name='month-challenge')
 ]
