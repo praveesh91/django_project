6016908ac11f9324e6e04ad470e8c65fbf296e6b
praveesh91 <praveesh91@gmail.com>
2025-06-26 00:07:02 +0530

    add 404 page

--- monthly_challanges/challanges/views.py
+++ monthly_challanges/challanges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 monthly_challanges = {
@@ -44,4 +44,4 @@ def monthly_challange(request, month):
             "month_name": month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+       raise Http404()
